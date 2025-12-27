bd27a24a1b44166876d094f916e2b6f21bd3f340
Jawaad Bhatti <javb222@hotmail.com>
2025-12-27 23:41:34 +0000

    Solved problem 6, part 2

--- problem_06.py
+++ problem_06.py
@@ -2,6 +2,33 @@ import numpy as np
 from helpers import load_text
 
 
+def solve2(fp):
+    txt = list(filter(lambda line: len(line) != 0, load_text(fp).split("\n")))
+    arr = np.array([list(l) for l in txt])[:, ::-1].transpose()
+    arr = arr[~np.all(arr == " ", axis=1), :]
+
+    problems: list[list[int], str] = []
+
+    acc: list[int] = []
+    is_last = False
+    for l in ["".join(l) for l in arr]:
+        l = l.strip()
+        match l[-1]:
+            case "*" | "+":
+                num = int(l[:-1])
+                is_last = True
+            case _:
+                num = int(l)
+
+        acc += [num]
+        if is_last:
+            problems += [[acc, l[-1]]]
+            acc = []
+            is_last = False
+
+    return sum(np.sum(p[0]) if p[-1] == "+" else np.prod(p[0]) for p in problems)
+
+
 def solve(fp):
     txt = load_text(fp).strip().split("\n")
     txt_nums = txt[:-1]
@@ -27,6 +54,9 @@ def solve(fp):
 def main():
     assert solve("p06_example.txt") == 4277556
     assert solve("p06_data.txt") == 6299564383938
+
+    assert solve2("p06_example.txt") == 3263827
+    assert solve2("p06_data.txt") == 11950004808442
 
 
 main()
