34aee9cbd0fae00ebe8c0af9fd8e6bd27a610277
Jawaad Bhatti <javb222@hotmail.com>
2025-12-27 17:32:09 +0000

    Solved problem 03, part 2

--- problem_03.py
+++ problem_03.py
@@ -9,14 +9,32 @@ def max_joltage(bank: str):
     return lmax * 10 + rmax
 
 
-def main():
+def max_12_digit_joltage(bank: str):
+    bank = list(map(int, bank))
 
-    # Part 1
+    res: list[int] = []
+    i_left = -1
+    for i in range(11, -1, -1):
+        m = bank[i_left + 1 : None if i == 0 else -i]
+
+        n_max = max(m)
+        i_left = m.index(n_max) + i_left + 1
+        res.append(n_max)
+
+    return sum(r * 10**p for r, p in zip(res, range(11, -1, -1)))
+
+
+def main():
     banks = load_text("p03_example.txt").strip().split("\n")
     assert sum(map(max_joltage, banks)) == 357
+    assert sum(map(max_12_digit_joltage, banks)) == 3121910778619
 
     banks = load_text("p03_data.txt").strip().split("\n")
+
+    # Part 1
     assert sum(map(max_joltage, banks)) == 17321
+    # Part 2
+    assert sum(map(max_12_digit_joltage, banks)) == 171989894144198
 
 
 main()
